940bcc6928c177078489ce2e3765a33c90eef0dc
trantuvan <trantuvan.kan@gmail.com>
2022-05-22 13:15:30 +0700

    Reverse String Done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  if (string === "") {
+    return "";
+  }
+  //*1 convert string to array
+  const stringArray = string.split("");
+  // *2 reverse array
+  stringArray.reverse();
+  //*3 convert array to string
+  const reverseString = stringArray.join("");
+  return reverseString;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// reverse String
+// input string
+// output string reverse
