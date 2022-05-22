d5c4df6bf647ecc31b3c4900b300d6e4184d5fc2
trantuvan <trantuvan.kan@gmail.com>
2022-05-22 11:28:07 +0700

    repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,21 @@
-const repeatString = function() {
+const repeatString = function (repeatString, repeatTime) {
+  let result = "";
 
+  if (repeatTime === 0 || repeatString === "") {
+    return result;
+  }
+
+  if (repeatTime < 0) {
+    return (result = "ERROR");
+  }
+
+  // *1 loop through repeatTime
+  for (let i = 0; i < repeatTime; i++) {
+    // *2 concat repeatString
+    result += repeatString;
+  }
+  // *3 return result
+  return result;
 };
 
 // Do not edit below this line
