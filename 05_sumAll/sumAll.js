e653d1b118f7c2bb64b2421cf9312973308722b2
trantuvan <trantuvan.kan@gmail.com>
2022-05-23 10:26:06 +0700

    Sum all integers
    Sum all numbers between 2 arbitrary given numbers.
    
    To avoid looping between min --> max values.
    Sum consecutive values formula has been used.
    
    As a result:
    - Sum all consecutive between 2 arbitrary numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  let sum = 0;
 
+  //Check for negative numbers
+  if (
+    number1 < 0 ||
+    number2 < 0 ||
+    typeof number1 !== "number" ||
+    typeof number2 !== "number"
+  ) {
+    return "ERROR";
+  }
+  const min = Math.min(number1, number2);
+  const max = Math.max(number1, number2);
+  const distance = Math.abs(max - min) + min;
+
+  sum = (distance * (max + min)) / 2;
+  return sum;
 };
 
 // Do not edit below this line
