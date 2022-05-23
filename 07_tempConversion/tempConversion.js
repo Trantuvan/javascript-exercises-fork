0369a6cd1f4cc7a6b3f77e68af28adce7902959d
trantuvan <trantuvan.kan@gmail.com>
2022-05-23 11:37:55 +0700

    Convert Temp DOne

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,24 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  if (fahrenheit === 32) {
+    return 0;
+  }
+  const celcius = ((fahrenheit - 32) * 5) / 9;
+  const oneDecimalPlaces = parseFloat(celcius.toFixed(1));
+  return oneDecimalPlaces;
 };
 
-const ctof = function() {
+const ctof = function (celcius) {
+  if (celcius === 0) {
+    return 32;
+  }
 
+  const fahrenheit = (celcius * 9) / 5 + 32;
+  const oneDecimalPlaces = parseFloat(fahrenheit.toFixed(1));
+  return oneDecimalPlaces;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
