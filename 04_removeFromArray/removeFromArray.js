7189d1c8a367552db87d44758b6359929a5c056e
trantuvan <trantuvan.kan@gmail.com>
2022-05-23 09:26:56 +0700

    Remove From Array
    Input 2 array origin & removal Array. Return new array not contain
    elements in removal array.
    
    Notice: shift focus away from verb "Remove".
    - Filter out elements in origins array that "included" in removal
    - Select those in origins array that not "included" in removal
    
    As a Result:
    - New array not contain elements in removal array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function (origins, ...removals) {
+  for (const removeElem of removals) {
+    // *1 Check origins contains elem of removals --> return index
+    let indexOfOrigins = origins.indexOf(removeElem);
 
+    // *2 if yes --> indexOf removalElem
+    if (indexOfOrigins > -1) {
+      // *3 remove that elem from origins
+      origins.splice(indexOfOrigins, 1);
+    }
+  }
+  return origins;
 };
+// const removeFromArray = function (...args) {
+//   const array = args[0];
+//   const newArray = [];
+
+//   // * remove only elements that are contained both in origins and removal array
+//   array.forEach((item) => {
+//     if (!args.includes(item)) {
+//       newArray.push(item);
+//     }
+//   });
+//   return newArray;
+// };
 
 // Do not edit below this line
 module.exports = removeFromArray;
